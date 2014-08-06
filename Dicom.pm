cb14b60b038328e6b6cc0dfaea0aa61046846418
Michal Špaček <tupinek@gmail.com>
2014-08-06 10:22:26 +0200

    First version.

---
+++ Dicom.pm
@@ -0,0 +1,26 @@
+package File::Find::Rule::Dicom;
+
+# Pragmas.
+use base qw(File::Find::Rule);
+use strict;
+use warnings;
+
+# Modules.
+use Dicom::File::Detect qw(dicom_detect_file);
+
+# Version.
+our $VERSION = 0.01;
+
+# Detect DICOM file.
+sub File::Find::Rule::dicom_file {
+	my $file_find_rule = shift;
+	my $self = $file_find_rule->_force_object;
+	return $self->exec(sub{
+		my $file = shift;
+		return dicom_detect_file($file);
+	});
+}
+
+1;
+
+__END__
